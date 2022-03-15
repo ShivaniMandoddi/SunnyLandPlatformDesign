88715daeb5b00c411b604f0c14d86958499ae487
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-03-15 21:34:36 +0530

    Level UI designing done

---
+++ Assets/BackgroundMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed*Time.deltaTime);
+        if (transform.position.x < -25.5)
+            transform.position = new Vector2(30f, transform.position.y);
+    }
+}
